73d374ed13672e50f2ed51c2178a203775a9451d
Balla <ballaphanindra@outlook.com>
2022-06-23 18:28:51 +0530

    Committing UC1

---
+++ Day10AddressBook/Day10AddressBook/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class AddressBook
+    {
+        public String firstName;
+        public String lastName;
+        public String address;
+        public String city;
+        public String state;
+        public long zip;
+        public long phoneNumber;
+        public String email;
+
+        static public void SetAddress()
+        {
+
+            AddressBook addressBook = new AddressBook();
+            addressBook.firstName = "Balla";
+            addressBook.lastName = "Phanindra";
+            addressBook.address = "Prashanth Nagar";
+            addressBook.city = "MSiddipet";
+            addressBook.state = "TS";
+            addressBook.zip = 502103;
+            addressBook.phoneNumber = 6789009876;
+            addressBook.email = "ballaphanindra@gmail.com";
+        }
+    }
+}
