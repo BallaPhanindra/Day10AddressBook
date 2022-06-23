76533cea29ceafadcda9590bf9de24833e122c3e
Balla <ballaphanindra@outlook.com>
2022-06-23 18:26:08 +0530

    initial commit

---
+++ Day10AddressBook/Day10AddressBook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class AddressBookMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program in AddressBookMain class on Master Branch");
+        }
+    }
+}
